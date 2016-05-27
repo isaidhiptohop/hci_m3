c0d4354e8017e1a20a47b7a7a6d545c2873e5036
Richard Paul <a1302451@unet.univie.ac.at>
2016-05-27 18:47:44 +0200

    updated the speaker group list, it now shows some entries, u can add speakers which are not locked to a group, but they wont be displayed. u can also add the same speaker several times to the same group, which is pretty stupid

--- hci_m3/www/js/speakers.js
+++ hci_m3/www/js/speakers.js
@@ -1,23 +1,42 @@
-var addspeakerprompt =
-  "<label for=\"speakername\">Speaker Name</label>" + 
-  "<input type=\"text\" id=\"speakername\"><br>" + 
-  "<input type=\"submit\" id=\"addspeakersubmit\" value=\"Submit\">";
-
 var speakerView = localStorage.getItem ('speakerview');
-var speakers = JSON.parse(localStorage.getItem ('speakers'));
 
-if (speakers == null) {
+
+
+var buf = localStorage.getItem('speakers');
+var speakers;
+
+if (buf == "[object Object]") alert ("looks like u forgot to stringify ur objects before saving them to local storage");
+
+if (buf == "" || buf == null) {
   speakers = [];
-  localStorage.setItem ('speakers', speakers);
+  localStorage.setItem ('speakers', JSON.stringify(speakers));
   console.log ("set speakers to empty array");
+} else {
+  speakers = JSON.parse(localStorage.getItem ('speakers'));
 }
+
+
+
+var buf = localStorage.getItem('groups');
+var groups;
+
+if (buf == "[object Object]") alert ("looks like u forgot to stringify ur objects before saving them to local storage");
+
+if (buf == "" || buf == null) {
+  groups = [];
+  localStorage.setItem ('groups', JSON.stringify(speakers));
+  console.log ("set groups to empty array");
+} else {
+  groups = JSON.parse(localStorage.getItem ('groups'));
+}
+
+
 
 if (speakerView == "allspeakers") {
   document.getElementById("groupedSpeakers").innerHTML = "";
   document.getElementById("addspeaker").addEventListener('click', addSpeaker);
   
   var speakerlist = "<table><tr><td>Speaker name</td><td>is playing</td><td>is locked</td><td></td><td></td></tr>"
-  
   for (var i = 0; i < speakers.length; i++) {
     speakerlist += "<tr><td>" + speakers[i].speakername + "</td>";
     speakerlist += "<td>" + speakers[i].isPlaying + "</td>";
@@ -28,27 +47,58 @@ if (speakerView == "allspeakers") {
     } else {
       speakerlist += "<td><button id=\"toggleplay_" + i + "\">Play</button></td>";
     }
-    document.getElementById("toggleplay_" + i).addEventListener('click', 
-      function () {
-        togglePlay (i);
-      }
-    );
-
+    console.log (document.getElementById("toggleplay_" + i));
     if (speakers[i].isLocked) {
       speakerlist += "<td><button id=\"lock_" + i + "\">Unlock</button></td>";
     } else {
       speakerlist += "<td><button id=\"lock_" + i + "\">Lock</button></td>";
     }
-    document.getElementById("lock_" + i).addEventListener('click', );
 
     speakerlist += "</tr>";
   }
   
-  speakerlist += "</ul>";
+  speakerlist += "</table>";
   document.getElementById("speakerlist").innerHTML = speakerlist;
+
+  for (var i = 0; i < speakers.length; i++) {
+    document.getElementById("toggleplay_" + i).addEventListener('click', 
+      function () {
+        var i = this.id.substring(this.id.length-1, this.id.length);
+        speakers[i].isPlaying = !speakers[i].isPlaying;
+        localStorage.setItem('speakers', JSON.stringify(speakers));
+        var buf = localStorage.getItem('speakers');
+        window.location.href = 'speakers.html';
+      }
+    );
+// */
+
+    document.getElementById("lock_" + i).addEventListener('click', 
+      function () {
+        var i = this.id.substring(this.id.length-1, this.id.length);
+        speakers[i].isLocked = !speakers[i].isLocked;
+        localStorage.setItem('speakers', JSON.stringify(speakers));
+        var buf = localStorage.getItem('speakers');
+        window.location.href = 'speakers.html';
+      }
+    );
+
+// */
+  }
 } else {
   document.getElementById("allSpeakers").innerHTML = "";
+  document.getElementById("addgroup").addEventListener('click', addGroup);
+
+  var grouplist = "<table><tr><td>Group name</td><td></td><td></td></tr>"
+  for (var i = 0; i < groups.length; i++) {
+    grouplist += "<tr><td>" + groups[i].groupname + "</td>";
+    grouplist += "</tr>";
+  }
+  
+  grouplist += "</table>";
+  document.getElementById("grouplist").innerHTML = grouplist;
 }
+
+
 
 document.getElementById('allspeakersbutton').addEventListener('click', setSpeakerViewAll);
 document.getElementById('groupedspeakersbutton').addEventListener('click', setSpeakerViewGrouped);
@@ -65,11 +115,21 @@ function setSpeakerViewGrouped () {
   window.location.href = 'speakers.html';
 }
 
+
+
+var addspeakerprompt =
+  "<label for=\"speakername\">Speaker Name</label>" + 
+  "<input type=\"text\" id=\"speakername\"><br>" + 
+  "<input type=\"submit\" id=\"addspeakersubmit\" value=\"Submit\">";
+
+// adds a speaker by replacing an empty div called 'addspeakerdiv' with a string called 'addspeakerprompt' which displays an input field for the new speaker
+// also adds an eventlistener to the new input field which calls saveSpeakerData ()
 function addSpeaker () {
   document.getElementById('addspeakerdiv').innerHTML = addspeakerprompt;
   document.getElementById('addspeakersubmit').addEventListener('click', saveSpeakerData);
 }
 
+// creates a new speaker object, which is added to speakers-array, which then will be stringified and saved in localstorage
 function saveSpeakerData () {
   var speakerName = document.getElementById("speakername").value;
   
@@ -87,11 +147,87 @@ function saveSpeakerData () {
   } else {
     speakers = new Array(newSpeaker);
   }
-//  console.log (speakers.length);
-//  console.log (speakers[0]);
-//  console.log (JSON.stringify(speakers));
   localStorage.setItem ('speakers', JSON.stringify(speakers));
-//  var speakers2 = JSON.parse(localStorage.getItem ('speakers'))
-//  console.log (speakers2[0]);
-//  console.log (speakers2.length);
 }
+
+
+
+function getaddgroupprompt () {
+  var prompt = "<label for=\"groupname\">Group Name</label>" + 
+    "<input type=\"text\" id=\"groupname\"><br>" +
+    "<div><table>";
+  
+  for (var i = 0; i < speakers.length; i++) {
+    prompt += "<tr>";
+    prompt += "<td>" + speakers[i].speakername + "</td>";
+    if (speakers[i].isLocked) {
+      prompt += "<td>Locked</td>";
+    } else {
+      prompt += "<td><button id=\"speaker_" + i + "\">Add</button></td>";
+    }
+    prompt += "</tr>";
+  }
+  
+  prompt += "</table></div><br>";  
+  prompt += "<input type=\"submit\" id=\"addgroupsubmit\" value=\"Submit\">";
+  
+  return prompt;
+}
+
+function addGroup () {
+  document.getElementById('addgroupdiv').innerHTML = getaddgroupprompt ();
+  for (var i = 0; i < speakers.length; i++) {
+    if (!speakers[i].isLocked) {
+      document.getElementById("speaker_" + i).addEventListener('click', addSpeakerToGroup);
+    }
+  }
+  document.getElementById('addgroupsubmit').addEventListener('click', saveGroupbuf);
+}
+
+function addSpeakerToGroup () {
+  var groupbuf;
+  
+  if (localStorage.getItem('groupbuf') == "" || localStorage.getItem('groupbuf') == null) {
+    groupbuf = {groupname: "", speakers: []};
+    localStorage.setItem ('groupbuf', JSON.stringify(groupbuf));
+    console.log ("set groupbuf to empty group object");
+  } else {
+    groupbuf = JSON.parse(localStorage.getItem ('groupbuf'));
+  }
+  
+  groupbuf.speakers.push(speakers[this.id.substring(this.id.length-1, this.id.length)].speakername);
+  localStorage.setItem ('groupbuf', JSON.stringify(groupbuf));
+  console.log (JSON.stringify(groupbuf));
+}
+
+function saveGroupbuf () {
+  var groupName = document.getElementById("groupname").value;
+  
+  if (groupName == "") {
+    alert ("Enter a name for your group");
+    return;
+  }
+  
+  var groupbuf;
+  
+  if (localStorage.getItem('groupbuf') == "" || localStorage.getItem('groupbuf') == null) {
+    groupbuf = {groupname: "", speakers: []};
+    localStorage.setItem ('groupbuf', JSON.stringify(groupbuf));
+    console.log ("set groupbuf to empty group object");
+  } else {
+    groupbuf = JSON.parse(localStorage.getItem ('groupbuf'));
+  }
+  
+  groupbuf.groupname = groupName;
+  
+  window.location.href = 'speakers.html';
+  
+  if (typeof groups == "object") {
+    groups.push(groupbuf);
+  } else {
+    groups = new Array(groupbuf);
+  }
+  localStorage.setItem ('groups', JSON.stringify(groups));
+  console.log(JSON.stringify(groups));
+}
+
